b9dda1ab037fc7309848914517294f44aea6ad8b
SHUBHAM SINGH <singh2407shubham@users.noreply.github.com>
2020-06-01 10:34:29 +0530

    download python two_fer

--- python/two-fer/two_fer.py
+++ python/two-fer/two_fer.py
@@ -1,4 +1,2 @@
-def two_fer(name=''):
-    if name == '' or name is None:
-        return "One for you, one for me."
-    return 'One for ' + name + ', ' + 'one for me.'
+def two_fer(name='you'):
+    return f"One for {name}, one for me."
